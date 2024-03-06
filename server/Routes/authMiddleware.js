15bf320ab839b282716ae24fb954914dcf54dc39
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-07 00:15:22 +0100

    auth/admin check middleware

---
+++ server/Routes/authMiddleware.js
@@ -0,0 +1,18 @@
+const isAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    console.log("user is authenticated");
+    next();
+  } else {
+    res.status(401).json({ err: "you are unauthorized to visit this" });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.isAuthenticated() && req.user.admin) {
+    console.log("user is admin", req.user.admin);
+    next();
+  } else {
+    res.status(401).json({ err: "you are unauthorized to visit admin page" });
+  }
+};
+module.exports = { isAdmin, isAuth };
