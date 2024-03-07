4efcf535ea732d0f3fbfaa757f01ee0ddc8c1425
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-07 12:20:52 +0100

    get all messages control

--- server/Controllers/MessageControllers.js
+++ server/Controllers/MessageControllers.js
@@ -3,16 +3,17 @@ const User = require("../Models/UserSchema");
 
 //check if user is authorized to post a message
 function messageAbility(user) {
-  if (user.admin === "true" || user.member === "true") {
+  if (user.admin === true || user.member === true) {
     return true;
   } else {
     return false;
   }
 }
 
+//post a message
 async function post_msg(req, res) {
   const { message } = req.body;
-  const sender = req.user._id.toString();
+  const sender = req.user.id;
   const user = await User.findById(sender).select("first_name last_name admin member");
   const msgAbility = messageAbility(user);
 
@@ -31,4 +32,42 @@ async function post_msg(req, res) {
   }
 }
 
-module.exports = { post_msg };
+//get all messages
+function get_allmsgs(req, res) {
+  const { admin, member } = req.user;
+
+  if (admin === false || member === false) {
+    messages
+      .find()
+      .then((allmsgs) => {
+        res.status(200).json(allmsgs);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  } else {
+    messages
+      .find()
+      .populate("user", "first_name last_name username createdAt")
+      .then((allmsgs) => {
+        res.status(200).json(allmsgs);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+}
+//delete a message
+function delete_msg(req, res) {
+  const { id } = req.params;
+  messages
+    .findByIdAndDelete(id)
+    .then((deleted) => {
+      res.status(200).json(deleted);
+    })
+    .catch((error) => {
+      next(error);
+    });
+}
+
+module.exports = { post_msg, get_allmsgs, delete_msg };
