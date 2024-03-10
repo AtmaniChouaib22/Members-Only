c1d8c4cbda2af2d1ef97f99dadc3fc67f246771f
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-10 18:06:58 +0000

    authent / memebership/ posts routing

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -1,43 +1,38 @@
 const passport = require("passport");
 const router = require("express").Router();
-const { register_user } = require("../Controllers/UserControllers");
-const { post_msg, get_allmsgs, delete_msg } = require("../Controllers/MessageControllers");
+const { register_user, get_user, set_member } = require("../Controllers/UserControllers");
+const { post_msg, get_allmsgs, delete_msg, get_allmsgs_protected } = require("../Controllers/MessageControllers");
 const { isAuth, isAdmin } = require("./authMiddleware");
 
 //Post routes
 
 router.post("/login", passport.authenticate("local"), (req, res) => {
-  res.redirect("/dashboard");
+  console.log(req.user);
+  console.log(req.session);
+  res.json(req.user);
 });
 
 router.post("/register", register_user);
 
 router.post("/newpost", isAuth, post_msg);
 
-router.post("/logout", function (req, res, next) {
+router.post("/logout", isAuth, function (req, res, next) {
   req.logout(function (err) {
     if (err) {
       return next(err);
+    } else {
+      res.status(200).json({ message: "logged out" });
     }
-    res.redirect("/");
   });
 });
 
 //Get routes
 
-router.get("/", get_allmsgs);
+router.get("/messages", get_allmsgs);
 
-router.get("/dashboard", isAuth, get_allmsgs);
+router.get("/protected", isAuth, get_allmsgs_protected);
 
-router.get("/login", (req, res, next) => {
-  const form =
-    '<h1>Login Page</h1><form method="POST" action="/login">\
-    Enter Username:<br><input type="text" name="email">\
-    <br>Enter Password:<br><input type="password" name="password">\
-    <br><br><input type="submit" value="Submit"></form>';
-
-  res.send(form);
-});
+router.patch("/member", isAuth, set_member);
 
 router.delete("/messages/:id", isAdmin, delete_msg);
 module.exports = router;
