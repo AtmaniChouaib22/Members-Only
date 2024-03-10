a6ec08373b6a029d82ae056f727d3913e641a8cb
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-10 18:09:05 +0000

    get all messages / protected controllers

--- server/Controllers/MessageControllers.js
+++ server/Controllers/MessageControllers.js
@@ -34,28 +34,26 @@ async function post_msg(req, res) {
 
 //get all messages
 function get_allmsgs(req, res) {
-  const { admin, member } = req.user;
+  messages
+    .find()
+    .then((allmsgs) => {
+      res.status(200).json(allmsgs);
+    })
+    .catch((error) => {
+      next(error);
+    });
+}
 
-  if (admin === false || member === false) {
-    messages
-      .find()
-      .then((allmsgs) => {
-        res.status(200).json(allmsgs);
-      })
-      .catch((error) => {
-        next(error);
-      });
-  } else {
-    messages
-      .find()
-      .populate("user", "first_name last_name username createdAt")
-      .then((allmsgs) => {
-        res.status(200).json(allmsgs);
-      })
-      .catch((error) => {
-        next(error);
-      });
-  }
+function get_allmsgs_protected(req, res) {
+  messages
+    .find()
+    .populate("user", "first_name last_name username createdAt admin member")
+    .then((allmsgs) => {
+      res.status(200).json(allmsgs);
+    })
+    .catch((error) => {
+      next(error);
+    });
 }
 //delete a message
 function delete_msg(req, res) {
@@ -70,4 +68,4 @@ function delete_msg(req, res) {
     });
 }
 
-module.exports = { post_msg, get_allmsgs, delete_msg };
+module.exports = { post_msg, get_allmsgs, delete_msg, get_allmsgs_protected };
