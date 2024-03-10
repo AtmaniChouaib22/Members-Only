557b9681c40aad045f680b14574ca5d3b92de325
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-10 18:08:17 +0000

    membership control

--- server/Controllers/UserControllers.js
+++ server/Controllers/UserControllers.js
@@ -1,12 +1,13 @@
 const User = require("../Models/UserSchema");
 const { genPassword, ValidateAdminRegistration } = require("../Utils/passwordUtils");
+require("dotenv").config();
 
 //Post routes
 
 const register_user = async (req, res, next) => {
   const { first_name, last_name, email, password, admin, adminPass } = req.body;
   const adminChecked = ValidateAdminRegistration(adminPass);
-  console.log(adminChecked);
+  console.log("check admin", adminChecked);
   //if the user is admin with wrong password
   if (admin === "true" && adminChecked === false) {
     const error = new Error("failed to register as an Admin wrong password");
@@ -41,4 +42,25 @@ const register_user = async (req, res, next) => {
   }
 };
 
-module.exports = { register_user };
+const set_member = (req, res, next) => {
+  const { secret_pass } = req.body;
+  console.log(req.user);
+  const email = req.user.email;
+  console.log("email", email);
+  if (secret_pass !== process.env.SECRET_PASS) {
+    console.log("wrong password for membership");
+    const error = new Error("failed to register as a member wrong password");
+    next(error);
+  } else {
+    User.findOneAndUpdate({ email }, { member: true }, { new: true })
+      .then((user) => {
+        console.log("membership user", user);
+        res.status(200).json(user);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+};
+
+module.exports = { register_user, set_member };
