30e2cadd7f7dbaafd4c1146f43f2dd5d046264f9
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-15 22:10:19 +0000

    card edited

--- client/src/components/custom/Card.tsx
+++ client/src/components/custom/Card.tsx
@@ -1,13 +1,16 @@
 import { Badge } from "@/components/ui/badge";
+import { useContext } from "react";
+import { appContext } from "@/App";
 
 const Card = ({ message, fullName, id, createdAt, admin, user }) => {
+  const { isLogged, setIsLogged } = useContext(appContext);
   return (
     <div
       key={id}
       className="flex flex-col gap-1 border-2 border-slate-950 rounded-md py-1 pl-2"
     >
       <div>{message}</div>
-      {user.member === true || user.admin === true ? (
+      {(user.member === true || user.admin === true) && isLogged ? (
         <div className="flex gap-2">
           <div>
             By <span className="font-bold pr-1">{fullName}</span>
